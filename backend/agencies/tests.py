3e7af420ccede1d029e4af79c1e23383e34518e8
abdi-edoc-de <se.abdulfeta.dedgba@gmail.com>
2023-03-19 15:53:20 +0300

    added more assert to a test_create_agency

--- backend/agencies/tests.py
+++ backend/agencies/tests.py
@@ -4,7 +4,6 @@ from rest_framework.test import APITestCase,force_authenticate,APIClient
 from admins.models import Admin, USER_TYPES
 
 
-#  - Abdulfeta is a returning intern from Google and a 5th year software engineering student. He has been an A2SV member since late 2021 and will be participating in the A2SV’s development efforts in this project as backend engineer. 
 import logging
 logger = logging.getLogger(__name__)
 
@@ -13,7 +12,6 @@ class PersonViewSetTests(APITestCase):
         logger.debug('Adding a new admin into database')
         self.admin = Admin(username='admin@gmail.com', password='admin1234', first_name = 'admin', last_name = 'admin', email = 'admin@gamil.com', user_type =  USER_TYPES[0][0])
         self.admin.save()
-        print()
         self.client = APIClient()
         self.client.force_authenticate(user=self.admin)
         logger.debug('Successfully added test admin into the database')
@@ -35,12 +33,19 @@ class PersonViewSetTests(APITestCase):
         }
         logger.debug('Sending TEST data to url: %s'%url)
         response = self.client.post(url,data)
-        # json = response.json()
+        json = response.json()
         logger.debug(response)
         logger.debug('Testing status code response: %s, code: %d'%(response.json(), response.status_code))
-        # print(response)
-        
+
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(json['name'] , json['name'])
+        self.assertEqual(data['url'] , json['url'])
+        self.assertEqual(data['lang'] , json['lang'])
+        self.assertEqual(data['phone'] , json['phone'])
+        self.assertEqual(str(data['admin']) , json['admin'])
+        logger.debug("The test_create_agency test has ended successfully")
+
+
 
 
 
