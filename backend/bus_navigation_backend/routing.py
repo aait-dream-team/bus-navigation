9e4b63cc5f505609f539c506ed99bd5f2a74e5de
Amanuel Debebe <amanueldebebe1000@gmail.com>
2023-04-03 23:39:42 +0300

    feat: implement notification consumer

--- backend/bus_navigation_backend/routing.py
+++ backend/bus_navigation_backend/routing.py
@@ -3,8 +3,12 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
+from notifications.urls import websocket_urlpatterns as notifications_websocket_urlpatterns
+
 
 websocket_urlpatterns = []
+
+websocket_urlpatterns += notifications_websocket_urlpatterns
 
 
 application = ProtocolTypeRouter({
