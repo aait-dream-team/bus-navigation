66c339bd4810e458c8346a69e5df7d20b91f82ea
abdi-edoc-de <se.abdulfeta.dedgba@gmail.com>
2023-04-04 13:22:34 +0300

    add updater app and implmented invoker for the notification

--- backend/bus_navigation_backend/urls.py
+++ backend/bus_navigation_backend/urls.py
@@ -18,6 +18,9 @@ from django.urls import path, include
 from admins import views as admin_views
 from rest_framework.authtoken import views as auth_views
 from rest_framework.routers import DefaultRouter
+from django.conf import settings
+from django.conf.urls.static import static
+
 from agencies import views as agency_views
 from routes import views as route_views
 from stops import views as stop_views
@@ -48,5 +51,5 @@ urlpatterns += [
     # path('', include(router.urls)),
     path('api-token-auth/', auth_views.obtain_auth_token),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
--- backend/updater/models.py
+++ backend/updater/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 from trips.models import Trip
 
-# Create your models here.
-class Update(models.Model):
-    pass
+from django.db.models.signals import post_save
+from channels.layers import get_channel_layer
 
-class VechicleUpdate(models.Model):
+
+class VehicleUpdate(models.Model):
     lat = models.FloatField()
     long = models.FloatField()
     trip = models.ForeignKey(to=Trip, on_delete=models.CASCADE)
     timestamp = models.TimeField()
 
 
-AFFECTED_ENTITY = (('r', 'route'), ('a', 'agency'), ('t', 'trip'))
+AFFECTED_ENTITY = (('route', 'route'), ('agency', 'agency'), ('trip', 'trip'))
 CAUSE = ((1, 'UNKNOWN_CAUSE'), (2, 'OTHER_CAUSE'), 
          (3, 'TECHNICAL_PROBLEM'), (4, 'STRIKE'), 
          (5, 'DEMONSTRATION'), (6, 'ACCIDENT'), 
@@ -24,10 +24,31 @@ EFFECT = ((1, 'NO_SERVICE'), (2, 'REDUCED_SERVICE'), (3, 'SIGNIFICANT_DELAYS'),
           (4, 'DETOUR'), (5, 'ADDITIONAL_SERVICE'), (6, 'MODIFIED_SERVICE'), 
           (7, 'OTHER_EFFECT'), (8, 'UNKNOWN_EFFECT'), (9, 'STOP_MOVED'))
 class Alert(models.Model):
-    affected_entity = models.CharField(choices=CAUSE)
+    affected_entity = models.CharField(choices=AFFECTED_ENTITY, max_length=40)
     entity_id = models.UUIDField() 
     cause = models.IntegerField(choices=CAUSE)
     effect = models.IntegerField(choices=EFFECT)
     duration = models.DurationField()
     start_timestamp = models.TimeField()
+
+
+def save_alert_profile(sender, instance, **kwargs):
+    channel_layer = get_channel_layer()
+    print('\n\nhi\n\n')
+    print(f'{instance.affected_entity}_{instance.entity_id}')
+    channel_layer.send(f'{instance.affected_entity}_{instance.entity_id}', {
+        'type': 'notify_update',
+        'message': instance
+    })
+
+def save_vehicle_update_profile(sender, instance, **kwargs):
+    channel_layer = get_channel_layer()
+    channel_layer.send(f'trip_{instance.trip.id}', {
+        'type': 'notify_update',
+        'message': instance    
+    })
+
+
+post_save.connect(save_alert_profile, sender=Alert)
+post_save.connect(save_vehicle_update_profile, sender=VehicleUpdate)
 
