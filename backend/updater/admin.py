1627630cd062b6c6cd5ef473568f8eab34aae448
Infinidrix <se.biruk.solomon@gmail.com>
2023-06-15 23:24:04 +0300

    add trip update logic to backend

--- backend/updater/admin.py
+++ backend/updater/admin.py
@@ -2,6 +2,7 @@ from bus_navigation_backend.new_admins import admin_site
 
 # Register your models here
 # .
-from .models import VehicleUpdate, Alert
+from .models import VehicleUpdate, Alert, TripUpdate
 admin_site.register(VehicleUpdate)
 admin_site.register(Alert)
+admin_site.register(TripUpdate)
