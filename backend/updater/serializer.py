d116336b88c2e28f41a6dec37c13a5daacd5034d
Natneam Mesele <se.natneam.mesele@gmail.com>
2023-06-15 10:41:41 +0300

    [fix] Alert fields error

--- backend/updater/serializer.py
+++ backend/updater/serializer.py
@@ -2,16 +2,16 @@ from rest_framework import serializers
 from .models import Alert, VehicleUpdate
 
 class AlertSerializer(serializers.ModelSerializer):
-    cause = serializers.SerializerMethodField()
-    effect = serializers.SerializerMethodField()
+    cause_field = serializers.SerializerMethodField()
+    effect_field = serializers.SerializerMethodField()
     class Meta:
         model = Alert
-        fields = ['affected_entity', 'cause', 'effect', 'duration', 'start_timestamp', 'agency_id', 'route_id', 'trip_id', 'message', 'alert_feed_id']
+        fields = ['affected_entity', 'cause', 'effect', 'effect_field', 'cause_field', 'duration', 'start_timestamp', 'agency_id', 'route_id', 'trip_id', 'message', 'alert_feed_id']
     
-    def get_cause(self, obj):
+    def get_cause_field(self, obj):
         return obj.get_cause_display()
     
-    def get_effect(self, obj):
+    def get_effect_field(self, obj):
         return obj.get_effect_display()
 
 class VehicleUpdateSerializer(serializers.ModelSerializer):
